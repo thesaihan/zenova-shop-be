c5f7fd9e55f8e6a1f7792d3487fe49549ff7c12f
Sai Han <thesaihan@gmail.com>
2021-03-20 10:59:49 +0630

    GET product from mongoDB

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,37 @@
 import express from "express";
-import products from "../data/products.js";
+import asyncHandler from "express-async-handler";
+import { Product } from "../model/index.js";
 
 export const productRoutes = express.Router();
 
-productRoutes.get("/", (req, res) => {
-  res.json(products);
-});
+/**
+ * @description Fetch all products
+ * @route GET /api/products
+ * @access Public
+ */
+productRoutes.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
-productRoutes.get("/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+/**
+ * @description Fetch product by ID
+ * @route GET /api/products/:id
+ * @access Public
+ */
+productRoutes.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const prodId = req.params.id;
+    if (!prodId.match(/^[0-9a-fA-F]{24}$/)) {
+      res.status(400).json({ message: "Invalid Product ID : " + prodId });
+    }
+    const product = await Product.findById(prodId);
+    product
+      ? res.json(product)
+      : res.status(404).json({ message: "Product Not Found : " + prodId });
+  })
+);
