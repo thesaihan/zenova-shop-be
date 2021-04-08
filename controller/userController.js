4d1fa5d0e3a949ba9cfc314a65ed3d5436c3551f
Sai Han <thesaihan@gmail.com>
2021-04-08 16:11:14 +0630

    Register new user

--- controller/userController.js
+++ controller/userController.js
@@ -40,3 +40,35 @@ export const getUserProfile = asyncHander(async (req, res) => {
   const user = await User.findById(req.user._id).select("-password");
   res.json(user);
 });
+
+/**
+ * @description Register new user
+ * @route POST /api/users
+ * @access Public
+ */
+export const registerNewUser = asyncHander(async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!(name && email && password)) {
+    res.status(400);
+    throw new Error("Missing user info");
+  }
+  const userExists = await User.findOne({ email }).select("-password");
+
+  if (!userExists) {
+    const createdUser = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password),
+    });
+    if (createdUser) {
+      createdUser.password = undefined;
+      res.json(createdUser);
+    } else {
+      res.status(500);
+      throw new Error("Failed to create user : " + email);
+    }
+  } else {
+    res.status(400);
+    throw new Error("User already exist : " + email);
+  }
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { processLogin, getUserProfile } from "../controller/index.js";
+import {
+  processLogin,
+  getUserProfile,
+  registerNewUser,
+} from "../controller/index.js";
 import { protectRoute } from "../middleware/index.js";
 
 export const userRoutes = express.Router();
 
+userRoutes.post("/", registerNewUser);
 userRoutes.post("/login", processLogin);
 userRoutes.route("/profile").get(protectRoute, getUserProfile);
