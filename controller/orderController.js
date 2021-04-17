26606b2f7537730a8f9fa1bef6e1dbe94936df92
Sai Han <thesaihan@gmail.com>
2021-04-17 13:09:10 +0630

    GET order by ID

--- controller/orderController.js
+++ controller/orderController.js
@@ -39,3 +39,27 @@ export const createNewOrder = asyncHander(async (req, res) => {
   });
   res.json(createdOrder);
 });
+
+/**
+ * @description Get order by ID
+ * @route GET /api/orders/:id
+ * @access Private
+ */
+export const getOrderById = asyncHander(async (req, res) => {
+  const orderId = req.params.id;
+  if (!orderId) {
+    res.status(400);
+    throw new Error("Missing Order ID");
+  } else if (!orderId.match(/^[0-9a-fA-F]{24}$/)) {
+    res.status(400);
+    throw new Error("Invalid Order ID : " + orderId);
+  } else {
+    const order = await Order.findById(orderId).populate("user", "name email");
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404);
+      throw new Error("Order not found : " + orderId);
+    }
+  }
+});
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { createNewOrder } from "../controller/index.js";
+import { createNewOrder, getOrderById } from "../controller/index.js";
 import { protectRoute } from "../middleware/index.js";
 
 export const orderRoutes = express.Router();
 
 orderRoutes.route("/").post(protectRoute, createNewOrder);
+orderRoutes.route("/:id").get(protectRoute, getOrderById);
